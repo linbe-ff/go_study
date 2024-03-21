6057e7de913582d5ac4c93bc57612f5e0f55ffed
WURUOHONG <wuruohong@zzfzyc.com>
2024-03-21 21:15:09 +0800

    sucess

--- colly/woyaogexing.go
+++ colly/woyaogexing.go
@@ -8,42 +8,81 @@ import (
 	"time"
 )
 
-func testCollyWoyaogexing() {
-	url := "https://www.pexels.com/zh-cn/search/wallpaper/"
-	cookieStr := "__cf_bm=A0m14BGt0xNbV3AMfFR2T1q6uKH7NkyiAPKfEXhpKaE-1711003121-1.0.1.1-oDoaKfDQLcyxXee2QVXU7U_Vc2iCBbx0vfxB2ZSSuO0cDZf2SVLwDPjUq3wtk6iVyVF400DyOChscNs8AQ9IXw; _sp_ses.9ec1=*; country-code-v2=US; _ga=GA1.1.1908980835.1711003130; cf_clearance=DaBd.niwavn_ntdv22xxhnkVOyTGOHV.O24gOns.kNQ-1711003128-1.0.1.1-hRD7r6A5_4tDl1pMRfxrbst97UOtexUckU9tfit5eo6FWALNeIGrJ8dBkLBq9lLbs0eRwIjWOrv1UzD0pJI_iQ; _fbp=fb.1.1711003136439.433319942; OptanonConsent=isGpcEnabled=0&datestamp=Thu+Mar+21+2024+14%3A41%3A46+GMT%2B0800+(%E4%B8%AD%E5%9B%BD%E6%A0%87%E5%87%86%E6%97%B6%E9%97%B4)&version=202301.1.0&isIABGlobal=false&hosts=&landingPath=NotLandingPage&groups=C0001%3A1%2CC0002%3A0%2CC0003%3A0%2CC0004%3A0&AwaitingReconsent=false; _sp_id.9ec1=eee1ba33-5d87-4600-b563-3057fa36f7bd.1711003129.1.1711003307..9d381904-e4cd-4e0d-ab27-2fba4217bbb7..67559036-6ecb-46d7-9194-0a6b9b1a38bf.1711003128869.6; _ga_8JE65Q40S6=GS1.1.1711003129.1.1.1711003307.0.0.0"
-	c := colly.NewCollector(
-		colly.Async(false),
-		colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0"),
-	)
+type (
+	PageInfo struct {
+		Title string
+		Href  string
+		Src   string
+	}
+	BaseInfo struct {
+		Cookie    string
+		Url       string
+		userAgent string
+	}
+	PageInfoList []*PageInfo
+)
 
-	// 添加一个具体的cookie，例如：
-	cookie := &http.Cookie{
+func Init(cookie, url, userAgent string) *BaseInfo {
+	return &BaseInfo{
+		Cookie:    cookie,
+		Url:       url,
+		userAgent: userAgent,
+	}
+}
+
+func (l *BaseInfo) InitCookie() *http.Cookie {
+	return &http.Cookie{
 		Name:    "cookie",
-		Value:   cookieStr,
-		Domain:  url,                            // 需要设置合适的域名
+		Value:   l.Cookie,
+		Domain:  l.Url,                          // 需要设置合适的域名
 		Path:    "/",                            // 设置路径，默认为"/"
 		Expires: time.Now().Add(24 * time.Hour), // 设置过期时间
 	}
-	c.SetCookies(url, []*http.Cookie{cookie})
+}
 
-	c.Limit(&colly.LimitRule{DomainGlob: "*.woyaogexing.*", Parallelism: 1})
+func AllPageInfo() {
+	var (
+		pageInfoList = make(PageInfoList, 0)
+	)
+	baseInfo := Init("acw_tc=3ccdc14317110130576548934e6a617df6034d445d08e68d0ca9770882fcce; PHPSESSID=klct3h61l6sn29qlknqqr7i4t6; saw_terminal=default; UM_distinctid=18e60535a5127c-0992569951295-4c657b58-1fa400-18e60535a52a57; CNZZDATA1261553859=862871121-1711013059-https%253A%252F%252Flink.zhihu.com%252F%7C1711013059; _abfpc=7ca9a41165e72934d0f58094a6439fa41a64c1c4_2.0; cna=dcd62a1926c3e8901691942d3b8ae25d",
+		"https://www.dpm.org.cn/lights/royal.html",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0",
+	)
+	baseInfo.GetPageOne(pageInfoList)
+}
+
+func (l *BaseInfo) GetPageOne(pageInfoList PageInfoList) {
+
+	c := colly.NewCollector(
+		colly.Async(false),
+		colly.UserAgent(l.userAgent),
+	)
+	c.SetCookies(l.Url, []*http.Cookie{l.InitCookie()})
 
 	c.OnRequest(func(r *colly.Request) {
-		log.Println("Visiting11111111111", r.URL)
+		log.Println("Visiting: ", r.URL)
 	})
-
 	c.OnError(func(_ *colly.Response, err error) {
-		log.Println("Something went wrong2222222222:", err)
+		log.Println("Something went wrong: ", err)
 	})
 
-	c.OnHTML(".Link_link__mTUkz spacing_noMargin__Q_PsJ", func(e *colly.HTMLElement) {
-		log.Println(e.ChildAttr("a", "href"))
+	c.OnHTML(".pic", func(e *colly.HTMLElement) {
+		//log.Println(e.ChildAttr("a", "href"),
+		//	strings.TrimSpace(e.DOM.Find("img").Eq(0).Text()),
+		//)
+		A标签 := e.DOM.Find("a")
+		标签指向地址, _ := A标签.Attr("href")
+		图片标签 := A标签.Find("img")
+		// 输出biaoqianImg的src
+		图片标题, _ := 图片标签.Attr("title")
+		图片来源, _ := 图片标签.Attr("src")
+		pageInfoList = append(pageInfoList, &PageInfo{
+			Title: 图片标题,
+			Href:  标签指向地址,
+			Src:   图片来源,
+		})
 	})
 
-	//c.OnHTML(".paginator a", func(e *colly.HTMLElement) {
-	//	e.Request.Visit(e.Attr("href"))
-	//})
-
-	c.Visit(url)
+	c.Visit(l.Url)
 	c.Wait()
 }

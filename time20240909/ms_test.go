3f8bb3e698719060d90e8d8a53136464137cff1b
wurho <935549961@qq.com>
2024-09-09 16:34:08 +0800

    筆記

--- time20240909/ms_test.go
+++ time20240909/ms_test.go
@@ -1,7 +1,54 @@
 package time20240909
 
-import "testing"
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
 
 func TestMS(t *testing.T) {
+	var (
+		c1      = make(chan int, 10)
+		wg      = &sync.WaitGroup{}
+		product = 100
+	)
 
+	defer close(c1)
+	wg.Add(2)
+	// c
+	go func() {
+		for i := 0; i < 100; i++ {
+			c1 <- i
+		}
+		defer wg.Done()
+	}()
+
+	// p
+	go func() {
+		sum := 0
+		for val := range c1 {
+			sum++
+			if sum == product {
+				break
+			}
+			fmt.Println(val)
+		}
+		defer wg.Done()
+	}()
+	wg.Wait()
+}
+
+func TestMP(t *testing.T) {
+	var (
+		s1 = []int{1, 4, 2, 3, 7, 8, 6, 5}
+	)
+	count := len(s1)
+	for i := 0; i < count; i++ {
+		for j := i + 1; j < count; j++ {
+			if s1[i] < s1[j] {
+				s1[i], s1[j] = s1[j], s1[i]
+			}
+		}
+	}
+	fmt.Println(s1)
 }
